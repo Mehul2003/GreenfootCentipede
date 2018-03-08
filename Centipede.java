cf354575bd78ecc4624b234823132dbe1dbd7e6c
Mehul2003 <mehultailang@yahoo.com>
2018-03-07 17:23:11 -0800

    HW 1

--- Centipede.java
+++ Centipede.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Centipede here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Mehul Tailang 
+ * @version 1.0
  */
 public class Centipede extends Actor
 {
@@ -15,20 +15,26 @@ public class Centipede extends Actor
     int dx = 5;
     GreenfootImage img = getImage();
     int w2 = img.getWidth()/2;
+
     public void act() 
     {
         setLocation(getX() + dx, getY());
         if (getWorld().getWidth() < getX() + w2) {
             turn(90);
-            move(20);
+            move();
             turn(90);
             dx = -dx;
         }
         else if (getX() - w2 < 0) {
             turn(-90);
-            move(20);
+            move();
             turn(-90);
             dx = -dx;
         }
-    }    
+    } 
+
+    public void move() 
+    {
+        move(getWorld().getHeight() / 16);
+    }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -17,8 +17,8 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1, false); 
         Centipede centipede = new Centipede();
-        addObject(centipede, 10, 20);
+        addObject(centipede, w/24, h/16);
     }
 }

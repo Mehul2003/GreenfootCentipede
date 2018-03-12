5752b2ea3ab59319d2428f1e77fb520e05bc40dc
Mehul2003 <mehultailang@yahoo.com>
2018-03-11 20:00:13 -0700

    HW 2

--- Centipede.java
+++ Centipede.java
@@ -8,33 +8,69 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Centipede extends Actor
 {
+    public Centipede() 
+    {
+        getImage().scale(20, 20);
+    }
     /**
      * Act - do whatever the Centipede wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    int dx = 5;
+    int dx = 20;
     GreenfootImage img = getImage();
     int w2 = img.getWidth()/2;
 
     public void act() 
     {
-        setLocation(getX() + dx, getY());
-        if (getWorld().getWidth() < getX() + w2) {
+       setLocation(getX() + dx, getY());
+       if (getWorld().getWidth() < getX() + w2) 
+        {
             turn(90);
             move();
             turn(90);
             dx = -dx;
         }
-        else if (getX() - w2 < 0) {
+        else if (getX() - w2 < 0) 
+        {
             turn(-90);
             move();
             turn(-90);
             dx = -dx;
         }
+        
+        else if (isTouching(Mushroom.class)) 
+        {
+            if (dx < 0) 
+            {
+                turn(-90);
+                move();
+                turn(-90);
+                dx = -dx;
+            }
+            else 
+            {
+                turn(90);
+                move();
+                turn(90);
+                dx = -dx;
+            }
+        }                       
     } 
 
     public void move() 
     {
-        move(getWorld().getHeight() / 16);
+        move(20);
+    }
+    
+    public boolean hittingMushroom() 
+    {
+        if (getWorld().getObjectsAt(this.getX(), this.getY(), Mushroom.class) != null) 
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -8,8 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    int w = getWidth();
-    int h = getHeight();
+    final int w = getWidth();
+    final int h = getHeight();
+    final int NUM_CENTIPEDES = 12;
+    final int NUM_MUSHROOMS = 50;
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,8 +20,23 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1, false); 
-        Centipede centipede = new Centipede();
-        addObject(centipede, w/24, h/16);
+        super(400, 600, 1, false); 
+        for (int i = 0; i < NUM_CENTIPEDES; i++) 
+        {
+            Centipede centipede = new Centipede();
+            addObject(centipede,  2* i * centipede.getImage().getWidth()/2 + centipede.getImage().getWidth()/2, centipede.getImage().getHeight()/2);
+        }
+        for (int i = 0; i < NUM_MUSHROOMS; i++) 
+        {
+            Mushroom mushroom = new Mushroom();
+            int x = Greenfoot.getRandomNumber(20) * 20 + mushroom.getImage().getWidth()/2;
+            int y = (Greenfoot.getRandomNumber(28) + 1) * 20 + mushroom.getImage().getHeight()/2;
+            if (getObjectsAt(x, y, Mushroom.class) != null) 
+            {
+                x = Greenfoot.getRandomNumber(20) * 20 + mushroom.getImage().getWidth()/2;
+                y = (Greenfoot.getRandomNumber(28) + 1) * 20 + mushroom.getImage().getHeight()/2;
+            }
+            addObject(mushroom, x, y);
+        }
     }
 }
